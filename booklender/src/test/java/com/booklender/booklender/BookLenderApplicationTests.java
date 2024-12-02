13b43d74539eb11a171ed5374011bbc395bed70d
Lucas Carvalho Costa <teamlucas@aluno.ueg.br>
2024-12-02 19:30:12 -0300

    Criação do Projeto

---
+++ booklender/src/test/java/com/booklender/booklender/BookLenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booklender.booklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
